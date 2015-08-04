820173ebef95ecac9bfa74892175156933790cbb
Chris <basschristopherjam@gmail.com>
2015-08-04 11:36:51 -0500

    Update main.go
    Fixed a bug that would cause i to never be 2

--- 3_largest_prime_factor/main.go
+++ 3_largest_prime_factor/main.go
@@ -19,7 +19,7 @@ func main() {
 		if number%i == 0 { // If the current value i is a factor of number
 			number = number / i // Divide the number by i
 			topPrime = i        // Set topPrime to the current i value
-			i = 2               // Reset i to 2
+			i = 1               // Reset i to 1 which will then be incremented
 		}
 	}
 
