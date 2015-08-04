ac5865fbf5ec0ac3a20159dd3ca0afbdf7bb6785
Chris Forsyth <chrisjamesforsyth@gmail.com>
2015-08-03 23:04:44 -0500

    Added solution to problem 3.

---
+++ 3_largest_prime_factor/main.go
@@ -0,0 +1,27 @@
+// Project Euler Problem 3
+// The prime factors of 13195 are 5, 7, 13 and 29.
+//
+// What is the largest prime factor of the number 600851475143 ?
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	number := 600851475143
+	topPrime := 1
+
+	// The larges prime factor cannot be larger than the ceiling of the square root
+	for i := 2; i <= int(math.Ceil((math.Sqrt(600851475143)))); i++ {
+		if number%i == 0 { // If the current value i is a factor of number
+			number = number / i // Divide the number by i
+			topPrime = i        // Set topPrime to the current i value
+			i = 2               // Reset i to 2
+		}
+	}
+
+	fmt.Println(topPrime) // Print the current topPrime
+}
